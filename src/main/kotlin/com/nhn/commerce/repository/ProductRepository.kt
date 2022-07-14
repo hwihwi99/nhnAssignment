659f1c5226cd7441735f683d596a34a12e8aaade
hwihwi99 <chamgrace@naver.com>
2022-07-14 10:57:03 +0900

    feat : 코틀린 scope function, 확장 함수, default값 넣기

--- src/main/kotlin/com/nhn/commerce/repository/ProductRepository.kt
+++ src/main/kotlin/com/nhn/commerce/repository/ProductRepository.kt
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Repository
 import java.time.LocalDateTime
 import java.util.Optional
 
+/**
+ * 이게 MyBatis임 (처음에는 마이해빗인줄 알고 진짜 무슨 신기술인줄 알았다...ㅋㅋㅋ)
+ * 이거를 사용하면 쿼리문이랑 해서 같이 쉽게 사용가능하다던데 이왕 이렇게 한거 그냥 정리한번 해보기.
+ * */
 @Mapper
 @Repository
 interface ProductRepository {
@@ -20,10 +24,10 @@ interface ProductRepository {
     fun getLastIndex() : Int
 
     @Insert("insert into product(productName, registerYmdt, salePrice,updateYmdt) value(#{productName},#{registerYmdt},#{salePrice},#{updateYmdt})")
-    fun insertNewProduct(productName:String, registerYmdt:LocalDateTime, salePrice:Int, updateYmdt:LocalDateTime)
+    fun insertNewProduct(productName:String = "기본값 입력", registerYmdt:LocalDateTime = LocalDateTime.now(), salePrice:Int = 0, updateYmdt:LocalDateTime = LocalDateTime.now())
 
     @Update("update product set productName = #{productName}, salePrice = #{salePrice}, updateYmdt = now() where productNo =#{productNo}")
-    fun updateProduct(productNo: Int, productName: String, salePrice: Int)
+    fun updateProduct(productNo: Int, productName: String = "기본값 수정", salePrice: Int = 0)
 
     @Delete("delete from product where productNo = #{productNo}")
     fun deleteProduct(productNo: Int)
--- src/main/kotlin/com/nhn/commerce/service/ProductService.kt
+++ src/main/kotlin/com/nhn/commerce/service/ProductService.kt
@@ -17,21 +17,42 @@ class ProductService(
     fun getDetail(productNo:Int):Product = productRepository.getDetailProduct(productNo)
     fun saveProduct(newProduct: NewProduct) {
         try{
-            val productPrice:Int = newProduct.salePrice.toInt();
+            val inputNewProduct = newProduct.let {
+                "{${newProduct.productName} and ${newProduct.salePrice}}"
+            }
+            println("새로받은 입력 값 확인하기")
+            println(inputNewProduct)
+            println("----------------------------")
+
+            val productPrice:Int = newProduct.salePrice.toInt()
+            if(productPrice.isNonPositive())
+                throw Exception("가격은 양수여야 합니다.")
+
             productRepository.insertNewProduct(newProduct.productName, LocalDateTime.now(), productPrice, LocalDateTime.now())
         } catch (e:java.lang.NumberFormatException) {
-            println("Not Number")
+            throw Exception("Not Number : 가격 입력값은 숫자로 이뤄져야 합니다.")
         }
     }
 
     fun updateProduct(productNo: Int, modifyProduct: ModifyProduct) {
         try{
+            val inputModifyProduct = modifyProduct.let {
+                "{${modifyProduct.productName} and ${modifyProduct.salePrice}}"
+            }
+            println("수정할 입력 값 확인하기")
+            println(inputModifyProduct)
+            println("----------------------------")
+
             val productPrice:Int = modifyProduct.salePrice.toInt();
+            if(productPrice.isNonPositive())
+                throw Exception("가격은 양수여야 합니다.")
+
             productRepository.updateProduct(productNo, modifyProduct.productName, productPrice)
         } catch (e:java.lang.NumberFormatException) {
-            println("Not Number")
+            throw Exception("Not Number : 가격 입력값은 숫자로 이뤄져야 합니다.")
         }
     }
 
+    fun Int.isNonPositive() : Boolean = this < 0;
     fun deleteProduct(productNo: Int) = productRepository.deleteProduct(productNo);
 }
